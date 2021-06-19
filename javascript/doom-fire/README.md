6def0568cd9cc10d3db9eb34d0d54352eff52ddb
Jeff DiasDosSantos <jeff.diasdossantos@flexit.es>
2021-06-19 02:22:44 +0200

    Added doom fire implementation

---
+++ javascript/doom-fire/README.md
@@ -0,0 +1,3 @@
+# Doom fire
+This was based on the implementation by Filipe Deschamps.  
+[Tutorial completo de como programar o Fogo do DOOM (JavaScript puro)](https://www.youtube.com/watch?v=fxm8cadCqbs)
---
+++ javascript/doom-fire/script.js
@@ -0,0 +1,279 @@
+const firePixelsArray = [];
+const fireWidth = 60;
+const fireHeight = 60;
+
+const fireColorsPalette = [
+    {
+        "r":7,
+        "g":7,
+        "b":7
+    },
+    {
+        "r":31,
+        "g":7,
+        "b":7
+    },
+    {
+        "r":47,
+        "g":15,
+        "b":7
+    },
+    {
+        "r":71,
+        "g":15,
+        "b":7
+    },
+    {
+        "r":87,
+        "g":23,
+        "b":7
+    },
+    {
+        "r":103,
+        "g":31,
+        "b":7
+    },
+    {
+        "r":119,
+        "g":31,
+        "b":7
+    },
+    {
+        "r":143,
+        "g":39,
+        "b":7
+    },
+    {
+        "r":159,
+        "g":47,
+        "b":7
+    },
+    {
+        "r":175,
+        "g":63,
+        "b":7
+    },
+    {
+        "r":191,
+        "g":71,
+        "b":7
+    },
+    {
+        "r":199,
+        "g":71,
+        "b":7
+    },
+    {
+        "r":223,
+        "g":79,
+        "b":7
+    },
+    {
+        "r":223,
+        "g":87,
+        "b":7
+    },
+    {
+        "r":223,
+        "g":87,
+        "b":7
+    },
+    {
+        "r":215,
+        "g":95,
+        "b":7
+    },
+    {
+        "r":215,
+        "g":95,
+        "b":7
+    },
+    {
+        "r":215,
+        "g":103,
+        "b":15
+    },
+    {
+        "r":207,
+        "g":111,
+        "b":15
+    },
+    {
+        "r":207,
+        "g":119,
+        "b":15
+    },
+    {
+        "r":207,
+        "g":127,
+        "b":15
+    },
+    {
+        "r":207,
+        "g":135,
+        "b":23
+    },
+    {
+        "r":199,
+        "g":135,
+        "b":23
+    },
+    {
+        "r":199,
+        "g":143,
+        "b":23
+    },
+    {
+        "r":199,
+        "g":151,
+        "b":31
+    },
+    {
+        "r":191,
+        "g":159,
+        "b":31
+    },
+    {
+        "r":191,
+        "g":159,
+        "b":31
+    },
+    {
+        "r":191,
+        "g":167,
+        "b":39
+    },
+    {
+        "r":191,
+        "g":167,
+        "b":39
+    },
+    {
+        "r":191,
+        "g":175,
+        "b":47
+    },
+    {
+        "r":183,
+        "g":175,
+        "b":47
+    },
+    {
+        "r":183,
+        "g":183,
+        "b":47
+    },
+    {
+        "r":183,
+        "g":183,
+        "b":55
+    },
+    {
+        "r":207,
+        "g":207,
+        "b":111
+    },
+    {
+        "r":223,
+        "g":223,
+        "b":159
+    },
+    {
+        "r":239,
+        "g":239,
+        "b":199
+    },
+    {
+        "r":255,
+        "g":255,
+        "b":255
+    }
+];
+
+
+function start() {
+    createFireDataStructure();
+    createFireSource();
+    renderFire();
+    setInterval(calculateFirePropagation, 50);
+}
+
+function createFireDataStructure() {
+    const numberOfPixels = fireWidth * fireHeight;
+    for(let i = 0; i < numberOfPixels; i++) {
+        firePixelsArray[i] = 0;
+    }
+}
+
+function calculateFirePropagation() {
+    for(let column = 0; column <fireWidth; column++){
+        for(let row = 0; row < fireHeight; row++){
+            const pixelIndex = column + (fireWidth * row);
+            updateFireIntensityPerPixel(pixelIndex);
+        }
+    }
+    renderFire();
+}
+
+function updateFireIntensityPerPixel(currentPixelIndex){
+    const belowPixelIndex = currentPixelIndex + fireWidth;
+    if(belowPixelIndex >= fireWidth * fireHeight){
+        return;
+    }
+
+    const direction = document.querySelector('input[name="wind-direction"]:checked').value;
+    const fireLiveness = document.querySelector('input[type="range"]').value;
+
+
+    let decay = Math.floor(Math.random() * fireLiveness);
+    const belowPixelFireIntensity = firePixelsArray[belowPixelIndex];
+
+    let newFireIntensity = belowPixelFireIntensity - decay;
+    if(newFireIntensity<0){
+        newFireIntensity = 0;
+    }
+
+    
+    if(direction == 'l'){
+        decay = decay * -1;
+    }
+
+    firePixelsArray[currentPixelIndex+decay] = newFireIntensity;
+}
+
+function renderFire() {
+    let html = '<table cellpadding="0" cellspacing="0">';
+    for(let row = 0; row < fireHeight; row++){
+        html += '<tr>';
+
+        for(let column = 0; column <fireWidth; column++){
+            const pixelIndex = column + (fireWidth * row);
+            const fireIntensity = firePixelsArray[pixelIndex];
+
+            html += `<td class="pixel" style="background-color: rgb(${ getColorForIntensity(fireIntensity) })">`;
+            // html += fireIntensity;
+            html += '</td>';
+        }
+
+        html += '</tr>';
+    }
+    html += '</table>';
+
+    document.querySelector('#fireCanvas').innerHTML = html;
+}
+
+function getColorForIntensity(fireIntensity){
+    const color = fireColorsPalette[fireIntensity];
+    return `${color.r},${color.g},${color.b}`;
+}
+
+function createFireSource() {
+    for(let column = 0; column <= fireWidth; column++) {
+        const overflowFixelIndex = fireWidth * fireHeight;
+        const pixelIndex = (overflowFixelIndex-fireWidth) + column;
+
+        firePixelsArray[pixelIndex] = 36;
+    }
+}
+
+start();
