7134e001b79b465bb6e0697c8f9b556b5b29cd70
Jeff DiasDosSantos <jeff.diasdossantos@flexit.es>
2021-07-17 01:59:14 +0200

    started to play with mongodb

--- typescript/appointment-calendar/appointment-api/src/index.ts
+++ typescript/appointment-calendar/appointment-api/src/index.ts
@@ -1,11 +1,15 @@
 import express, { Application } from "express";
 import {routes} from "./routes"
+import { Mongoose } from "mongoose";
 
 const app: Application = express();
 const port = 3000;
-app.use(express.json());
 
+app.use(express.json());
 app.use('/api/v1/', routes);
+
+const mongoose = new Mongoose({ useNewUrlParser: true,  useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/appointments').then()
 
 try {
     app.listen(port, (): void => {
