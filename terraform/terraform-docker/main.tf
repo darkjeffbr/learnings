9f28586e196c828e8dce48db8aaf18ebe110d7a3
Jeff DiasDosSantos <jeff.diasdossantos@flexit.es>
2021-08-07 02:02:03 +0200

    added terraform random resource and playing with count, splat operator and for expression

--- terraform/terraform-docker/main.tf
+++ terraform/terraform-docker/main.tf
@@ -9,15 +9,35 @@ terraform {
 
 provider "docker" {}
 
+resource "random_string" "random" {
+  count = 2
+  length = 4
+  special = false
+  upper = false
+}
+
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
 resource "docker_container" "nodered_container" {
-  name  = "nodered-abc"
+  count = 2
+  name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    external = 1880
+    # external = 1880
   }
+}
+
+output "Ip-Address" {
+  value = [for container in docker_container.nodered_container[*]: join(":", [container.ip_address], container.ports[*].external)]
+
+  #join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
+  description = "The IP address and external port of the container"
+}
+
+output "Container-Name" {
+  value = docker_container.nodered_container[*].name
+  description = "The name of the container"
 }
