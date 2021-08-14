dbf431e866cbbab3ea8595af5004dc64baa7445d
Jeff DiasDosSantos <jeff.diasdossantos@flexit.es>
2021-08-15 00:37:43 +0200

    externilize values to env variables with dotenv

--- typescript/appointment-calendar/appointment-api/src/index.ts
+++ typescript/appointment-calendar/appointment-api/src/index.ts
@@ -1,15 +1,18 @@
+import dotenv  from "dotenv"
 import express, { Application } from "express";
 import {routes} from "./routes"
 import { Mongoose } from "mongoose";
 
+dotenv.config();
+
 const app: Application = express();
-const port = 3000;
+const port = process.env.SERVER_PORT;
 
 app.use(express.json());
 app.use('/api/v1/', routes);
 
 const mongoose = new Mongoose({ useNewUrlParser: true,  useUnifiedTopology: true});
-mongoose.connect('mongodb://localhost:27017/appointments').then()
+mongoose.connect(`mongodb://${process.env.DB_SERVER}:${process.env.DB_PORT}/appointments`).then()
 
 try {
     app.listen(port, (): void => {
