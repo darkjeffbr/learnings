277b278bb0079843f6c41465fe456c23cdf5b079
Jeff DiasDosSantos <jeff.diasdossantos@flexit.es>
2021-08-15 01:03:14 +0200

    Improved server startup

--- typescript/appointment-calendar/appointment-api/src/index.ts
+++ typescript/appointment-calendar/appointment-api/src/index.ts
@@ -12,13 +12,17 @@ app.use(express.json());
 app.use('/api/v1/', routes);
 
 const mongoose = new Mongoose({ useNewUrlParser: true,  useUnifiedTopology: true});
-mongoose.connect(`mongodb://${process.env.DB_SERVER}:${process.env.DB_PORT}/appointments`).then()
-
-try {
-    app.listen(port, (): void => {
-        console.log(`Connected successfully on port ${port}`);
-    });
-} catch (error) {
-    console.error(`Error occured: ${error.message}`);
-}
-
+console.log('Connecting to database ...');
+mongoose.connect(`mongodb://${process.env.DB_SERVER}:${process.env.DB_PORT}/appointments`).then(() => {
+    console.log('Connected successfully to database');
+    console.log('Starting up server ...');
+    try {
+        app.listen(port, (): void => {
+            console.log(`Server listening on port ${port}`);
+        });
+    } catch (error) {
+        console.error(`Error occured: ${error.message}`);
+    }
+}).catch(error => {
+    console.error(`Error occured: ${error}`);
+})
