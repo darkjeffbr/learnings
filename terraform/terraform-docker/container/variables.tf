dd1ba6721f4260297084f0685396fcb2e8930071
Jeff DiasDosSantos <jeff.diasdossantos@flexit.es>
2021-08-26 00:08:06 +0200

    created module container

---
+++ terraform/terraform-docker/container/variables.tf
@@ -0,0 +1,23 @@
+variable "name_in" {
+
+}
+
+variable "image_in" {
+
+}
+
+variable "int_port_in" {
+
+}
+
+variable "ext_port_int" {
+
+}
+
+variable "container_path_in" {
+
+}
+
+variable "host_path_in" {
+
+}
--- terraform/terraform-docker/outputs.tf
+++ terraform/terraform-docker/outputs.tf
@@ -1,10 +1,10 @@
-output "Ip-Address" {
-  value = [for container in docker_container.nodered_container[*]: join(":", [container.ip_address], container.ports[*].external)]
-  description = "The IP address and external port of the container"
-  #sensitive = true
-}
-
-output "Container-Name" {
-  value = docker_container.nodered_container[*].name
-  description = "The name of the container"
-}
+#output "Ip-Address" {
+#  value = [for container in docker_container.nodered_container[*]: join(":", [container.ip_address], container.ports[*].external)]
+#  description = "The IP address and external port of the container"
+#  #sensitive = true
+#}
+#
+#output "Container-Name" {
+#  value = docker_container.nodered_container[*].name
+#  description = "The name of the container"
+#}
